d2ecae322274ccefa5e6a5e9da5e7369be2f0fc7
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-03-07 16:31:16 +0200

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,21 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './events/entity/Event';
 import { EventsModule } from './events/events.module';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: 'env/.env' }),
     TypeOrmModule.forRoot({
       type: 'mariadb',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'example',
-      database: 'nest-events',
+      host: process.env['DB_HOST'],
+      port: parseInt(process.env['DB_PORT']),
+      username: process.env['DB_USERNAME'],
+      password: process.env['DB_PASSWORD'],
+      database: process.env['DB_NAME'],
       entities: [Event],
-      synchronize: true,
+      synchronize: process.env['DB_SYNCHRONIZE'] === 'true',
       logging: ['query', 'error'],
     }),
     EventsModule,
